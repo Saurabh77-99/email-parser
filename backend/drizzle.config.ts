c7653667b6937242a36381743607540e2f7bbc1d
Saurabh77-99 <saurabhdavda7799@gmail.com>
2026-04-02 00:33:15 +0530

    Turso connected!

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'sqlite',
+  dialect: 'turso',
   dbCredentials: {
     url: process.env.TURSO_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
